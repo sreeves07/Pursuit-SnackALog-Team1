bbe58619b2b1c187132ee8ada970c4f1e1e9a452
sreeves07 <shamarreeves@pursuit.org>
2023-01-30 14:37:22 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,9 @@ CREATE DATABASE snack_a_log;
 
 CREATE TABLE snacks (
     id SERIAL PRIMARY KEY, 
-    name TEXT, 
-    fiber INT DEFAULT 0, 
-    protein INT DEFAULT 0, 
+    name TEXT NOT NULL, 
+    fiber INT DEFAULT 0 NOT NULL, 
+    protein INT DEFAULT 0 NOT NULL, 
     added_sugar INT DEFAULT 0, 
     is_healthy BOOLEAN, 
     image TEXT
