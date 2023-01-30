d4efde0e383e63b578d42909c3309eebd9d67f36
sreeves07 <shamarreeves@pursuit.org>
2023-01-30 12:55:44 -0500

    deployed backend

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE snacks (
     id SERIAL PRIMARY KEY, 
     name TEXT, 
     fiber INT DEFAULT 0, 
-    protein INT, 
-    added_sugar INT, 
+    protein INT DEFAULT 0, 
+    added_sugar INT DEFAULT 0, 
     is_healthy BOOLEAN, 
     image TEXT
 );
