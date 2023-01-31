1467c3d596c6e9511a91f377544ab18f94c7d9b1
sreeves07 <shamarreeves@pursuit.org>
2023-01-31 15:51:44 -0500

    boolean and image helper function

--- validations/helperFunctions.js
+++ validations/helperFunctions.js
@@ -12,4 +12,25 @@ const checkName = (req, res, next) => {
     }
 };
 
+const checkBoolean = (req, res, next) => {
+    let health = req.body.is_healthy
+    if (typeof health === "boolean" ||  health === undefined) {
+        next()
+    } else {
+        res.status(400).json({ error: "is_healthy should be TRUE or FALSE"})
+    }
+}
+
+const checkImage = (req, res, next) => {
+    let image = req.body.image
+    if (!image) {
+        image = 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'
+    } else {
+        if (image.substring(0, 7) !== 'https://') {
+            res.status(400).json({ error: "Image must have valid https:// URL"})
+        }
+    }
+}
+
+
 module.exports = { checkName }
