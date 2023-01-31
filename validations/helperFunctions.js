f0e84457431713922979b50017c0957f3df4944e
sreeves07 <shamarreeves@pursuit.org>
2023-01-31 11:42:44 -0500

    checkName helper function working

--- validations/helperFunctions.js
+++ validations/helperFunctions.js
@@ -1,6 +1,12 @@
 const checkName = (req, res, next) => {
-    if (req.body.name.length >= 2) {
-        req.body.name = req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1).toLowerCase()
+    let input = req.body.name
+    input = input.trim().split(" ")
+    // test cases: 1. "Snack", 2. "snack" 3. "Snack pass" 4. "snack pass"
+    if (input.length) {
+        let newInput = input.map((word) => {
+            return word[0].toUpperCase() + word.substring(1)
+        }).join(" ")
+        req.body.name = newInput
         next()
     } else {
       res.status(400).json({ error: "Invalid Name Input" });
